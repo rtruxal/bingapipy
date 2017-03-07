2805d335f52e0e234413c5d6a5fa5c16f3940246
Rob <rtruxal@gmail.com>
2017-03-07 10:06:29 -0800

    got rid of default NoneTypes to make clearer that 'key' & 'q' vals are not optionally passed to the constructor

--- bingapipy_NO_PAGING.py
+++ bingapipy_NO_PAGING.py
@@ -1,3 +1,4 @@
+from errors_and_validations import JsonParsingError
 import requests
 from requests.models import urlencode
 from socket import gethostbyname, gethostname
@@ -5,6 +6,8 @@ from collections import OrderedDict
 from time import sleep
 
 
+
+__all__ = ['local_user_constants', 'BingSearch', 'decode_response_url']
 ###############################################
 ##                                           ##
 ##       User-defined dictionaries for       ##
@@ -18,8 +21,9 @@ class local_user_constants():
     ###############################################
     ## Enter default-header customizations here. ##
     ###############################################
-    HEADERS['User-Agent'] = "Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1"
-    HEADERS['X-Search-ClientIP'] = gethostbyname(gethostname())
+    ##HEADERS['Ocp-Apim-Subscription-Key'] = None                                                               # <--('Ocp-Apim-Subscription-Key' SHOULD NOT BE SET HERE. YOU MUST PASS IT TO THE SEARCH-OBJECT-CONSTRUCTOR CLASS: BingSearch)
+    HEADERS['User-Agent'] = "Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1" # <--(dummy User-Agent header for consistent response-format)
+    HEADERS['X-Search-ClientIP'] = gethostbyname(gethostname())                                                 # <--(these are methods from the 'socket' module which produce the host-machine's public IP)
     HEADERS['X-MSEdge-ClientID'] = None
     HEADERS['Accept'] = None
     HEADERS['Accept-Language'] = None
@@ -28,6 +32,7 @@ class local_user_constants():
     ###############################################
     ##     Enter query customizations here.      ##
     ###############################################
+    ##INCLUDED_PARAMS['q'] = None              # <--(BOTH THE 'Ocp-Apim-Subscription-Key' FROM 'HEADERS' AND THE 'q' FROM 'INCLUDED_PARAMS' MUST BE PASSED MANUALLY TO THE SEARCH-OBJECT-CONSTRUCTOR CLASS: BingSearch)
     INCLUDED_PARAMS['cc'] = None               # <--(See constants._COUNTRY_CODES below for available options)
     INCLUDED_PARAMS['count'] = "50"            # <--(Enter a number from 0-50. Must by type==str. EX: count of 5 should be "5")
     INCLUDED_PARAMS['freshness'] = None        # <--(Poss values are 'Day', 'Week', or 'Month')
@@ -38,20 +43,20 @@ class local_user_constants():
     INCLUDED_PARAMS['setLang'] = None          # <--(See ISO 639-1, 2-letter language codes here: https://www.loc.gov/standards/iso639-2/php/code_list.php)
     INCLUDED_PARAMS['textDecorations'] = None  # <--(Case-insensitive boolean. '(t|T)rue', or '(f|F)alse')
     INCLUDED_PARAMS['textFormat'] = None       # <--(Poss values are 'Raw', and 'HTML.' Default is 'Raw' if left blank.)
-
+    # News Search Only!
+    INCLUDED_PARAMS['category'] = None         # <--(ONLY FOR NEWS SEARCH. See available categories by mkt)
 
 ###############################################
 ##                                           ##
 ##      Primary API for SearchWebLite        ##
 ##                                           ##
 ###############################################
-class BingLite(object):
+class BingSearch(object):
 
     ###############################################
     ## Initialization functions and attr-setting ##
     ###############################################
-    def __init__(self, api_key, query, endpoint='web', verbose=True,
-                 params=local_user_constants.INCLUDED_PARAMS.copy() , headers=local_user_constants.HEADERS.copy()):
+    def __init__(self, api_key=None, query=None, endpoint='web', verbose=True, params=local_user_constants.INCLUDED_PARAMS.copy() , headers=local_user_constants.HEADERS.copy()):
         self._key = api_key
         self.query_plaintext = query
         self.params = params
@@ -67,10 +72,13 @@ class BingLite(object):
         self._url_comparisons = []
 
 
-    def _init_constructor_funcs(self):
+    def _init_constructor_funcs(self, validate_passed_params=False):
         self.base_url = local_static_constants.API_ENDPOINTS[self.endpoint_type]
         # encode your query to be URL-rdy
-        self._encoded_q = urlencode(dict(q=self.query_plaintext))
+        if 'categories' not in self.endpoint_type:
+            self._encoded_q = urlencode(dict(q=self.query_plaintext))
+        else:
+            self._encoded_q = self._handle_categorical_query()
         # clean out them' dictionary attrs.
         self.headers = _clear_null_vals(self.headers)
         self.params = _clear_null_vals(self.params)
@@ -78,7 +86,8 @@ class BingLite(object):
         self.headers = self._inject_key_into_header(self.headers)
         self.last_predicted_url = self._predict_url(bypass_setting_attrs=True)
         if self._verbose:
-            print 'The search-interface has been initialized w/ the following params:\n\nEndpoint-Type: {}\n\nQuery-URL: {}\n\nHeader-Dict: {}'.format(self.endpoint_type, self.last_predicted_url, self.headers)
+            print 'The search-interface has been initialized w/ the following params:\n\n\
+            Endpoint-Type: {}\n\nQuery-URL: {}\n\nHeader-Dict: {}'.format(self.endpoint_type, self.last_predicted_url, self.headers)
 
     ###############################################
     ##   _methods used BEFORE request is sent    ##
@@ -89,6 +98,19 @@ class BingLite(object):
         if not bypass_setting_attrs:
             self.last_predicted_url = prediction
         return prediction
+
+    def _handle_categorical_query(self):
+        """Essentially just a validation method. categorical search must be used in conjunction w/ the mkt param"""
+        # Must specify mkt param to do categorical searches. Only two working are for GB and US.
+        assert self.params['mkt'] != None and self.params['mkt'][-2:].lower() in ('us', 'gb')
+        if self.params['mkt'][-2:].lower() == 'us' and self.query_plaintext in local_static_constants.NEWS_CATEGORIES_US:
+            pass
+        elif self.params['mkt'][-2:].lower() == 'gb' and self.query_plaintext in local_static_constants.NEWS_CATEGORIES_GB:
+            pass
+        else:
+            raise ValueError('mkt param and categorical query term do not match')
+        return 'category={}'.format(self.query_plaintext)
+
 
     def _inject_key_into_header(self, header_dictionary, override=False):
         OD_w_key_added = OrderedDict()
@@ -127,8 +149,6 @@ class BingLite(object):
         # Handle error-codes and Warn about potential garbage results if query URL is too long.
         if len(response_object.url) > 1300:
             print('WARNING: URL too long at {} characters.\n Bing can silently truncate your query.\n Limit URLs to < 1,200 chars.').format(len(response_object.url))
-        # import pdb
-        # pdb.set_trace()
         response_validated = validate_request_response(response_object)
         if response_validated == '429':
             response_object = self._handle_429_error(url=response_object.url)
@@ -171,11 +191,44 @@ class BingLite(object):
         :return list of WebResult objects: parsed and prettied JSON results with easy data-access.
                 Returned as a LIST of WebResult objects with len == the # of links returned by Bing.
         """
-        if not self.total_estimated_matches and self.endpoint_type == 'web':
-            print(('Bing says there are an estimated {} results matching your query'.format(json_response['webPages']['totalEstimatedMatches'])))
-            self.total_estimated_matches = int(json_response['webPages']['totalEstimatedMatches'])
-        packaged_json = [WebResult(single_json_entry) for single_json_entry in json_response['webPages']['value']]
-        return packaged_json
+
+        ##TODO: MAKE THIS NOT HACKY
+
+        # Catch and handle error-responses
+        if 'webPages' not in json_response.keys():
+            try:
+                if bool(json_response['rankingResponse']) is False:
+                    if self._verbose:
+                        print('NO RESULTS RETURNED BY BING. RETURNING ORIGINAL JSON.')
+                    else: pass
+                    return json_response
+            except KeyError:
+                # print('unable to determine if empty, attempting WebResult extraction.')
+                pass
+            try:
+                link_list = json_response[self.params['responseFilter']]['value']
+                try:
+                    return [WebResult(single_json_entry) for single_json_entry in link_list]
+                except Exception:
+                    print('unrecognized response format.\n RETURNING LIST OF URLS, NOT WEBRESULT OBJECTS.')
+                    return [json_item['url'] for json_item in link_list]
+            except KeyError:
+                try:
+                    link_list = json_response['value']
+                    return [single_json_entry['url'] for single_json_entry in link_list]
+                except KeyError:
+                    raise EnvironmentError('something is wrong with using responsefilter to id the json.\n aka I"m a bad coder')
+        elif json_response['_type'] == 'News':
+
+            return [NewsResult(single_json_entry) for single_json_entry in json_response['value']]
+        elif json_response['_type'] == 'SearchResponse':
+            if not self.total_estimated_matches:
+                print(('Bing says there are an estimated {} results matching your query'.format(
+                    json_response['webPages']['totalEstimatedMatches'])))
+                self.total_estimated_matches = int(json_response['webPages']['totalEstimatedMatches'])
+            packaged_json = [WebResult(single_json_entry) for single_json_entry in json_response['webPages']['value']]
+            return packaged_json
+        else: raise JsonParsingError('_parse_json in bingapipy did not parse correctly')
 
     def _handle_429_error(self, url):
         timeout_cnt = 0
@@ -193,7 +246,6 @@ class BingLite(object):
             else:
                 raise IOError(local_static_constants._ERROR_CODES['429'])
         return r2
-
 
 
 ###############################################
@@ -229,18 +281,44 @@ class WebResult(object):
         # maintain compatibility
         self.title = result.get('name')
         self.description = result.get('snippet')
-        if self.url:
-            self.url_decoded = decode_response_url(self.url)
 
-    def __repr__(self):
+    def __str__(self):
         return 'WebResponse Obj: {}'.format(self.display_url)
 
-    @property
-    def get_dict(self):
-        return self.__dict__
+    def __repr__(self):
+        return '{}'.format(self.display_url)
 
 
+class NewsResult(object):
+    def __init__(self, result):
+        try:
+            self.about_name = result.get('about')[0]['name']
+            self.about_readlink = result.get('about')[0]['readLink']
+        except Exception:
+            self.about_name = None
+            self.about_readlink = None
+        try:
+            self.image_url = result.get('image')['thumbnail']['contentUrl']
+            self.image_width = result.get('image')['thumbnail']['width']
+            self.image_height = result.get('image')['thumbnail']['height']
+        except Exception:
+            self.image_url = None
+            self.image_width = None
+            self.image_height = None
+        try:
+            self.provider_type = result.get('provider')[0]['_type']
+            self.provider_name = result.get('provider')[0]['name']
+        except Exception:
+            self.provider_type = None
+            self.provider_name = None
+        self.category = result.get('category')
+        self.name = result.get('name')
+        self.date_published = result.get('datePublished')
+        self.description = result.get('description')
+        self.url = result.get('url')
 
+    def __str__(self):
+        return 'NewsResult'
 ###############################################
 ##                                           ##
 ##          class-independent funcs          ##
@@ -252,27 +330,12 @@ def _clear_null_vals(dictionary):
 
 
 def decode_response_url(bing_encoded_url):
-    """
-    THIS FUNCTION WILL PREVENT YOU FROM GETTING TAILORED RESULTS FROM BING.
-
-    :param bing_encoded_url: A response-url from bing -- str
-    :return: a decoded response-url from bing -- str
-    """
-    import urllib
+    import binascii
+    # yes it appears to be this easy.
     new_url = bing_encoded_url[153:-15].lstrip('=')
-    ans = urllib.unquote(new_url).decode('utf8')
-    # if, for any reason, urllib.unquote() fails, try again manually with binascii:
-    if ans == bing_encoded_url[153:-15].lstrip('='):
-        import binascii
-        # yes it appears to be this easy.
-        new_url = bing_encoded_url[153:-15].lstrip('=')
-        new_new_url = new_url.replace('%3a', binascii.a2b_hex('3a'))
-        new_url = new_new_url.replace('%2f', binascii.a2b_hex('2f'))
-        new_new_url = new_url.replace('%3f', binascii.a2b_hex('3f'))
-        ans2 = new_new_url.replace('%3d', binascii.a2b_hex('3d'))
-        return ans2
-    else: return ans
-
+    new_new_url = new_url.replace('%3a', binascii.a2b_hex('3a'))
+    new_new_new_url = new_new_url.replace('%2f', binascii.a2b_hex('2f'))
+    return new_new_new_url
 
 def validate_request_response(response):
     """
@@ -310,6 +373,7 @@ class local_static_constants():
         'videos_trending': 'https://api.cognitive.microsoft.com/bing/v5.0/videos/trending/search?',
         'videos_details': 'https://api.cognitive.microsoft.com/bing/v5.0/videos/details/search?',
         'news': 'https://api.cognitive.microsoft.com/bing/v5.0/news/search?',
+        'news_categories': 'https://api.cognitive.microsoft.com/bing/v5.0/news?',
         'news_trending': 'https://api.cognitive.microsoft.com/bing/v5.0/news/trendingtopics&'
     }
     _ERROR_CODES = {
@@ -321,3 +385,45 @@ class local_static_constants():
         '410': 'The request was made using HTTP. Only HTTPS is supported.(_BASE_ENDPOINT USES HTTPS. EITHER YOU CHANGED THAT OR YOU ARE NOT AT FAULT)',
         '429': 'The user exceeded their queries per second quota',
     }
+    NEWS_CATEGORIES_US = (
+        'Business',
+        'Entertainment',
+        'Entertainment_MovieAndTV',
+        'Entertainment_Music',
+        'Health',
+        'Politics',
+        'ScienceAndTechnology',
+        'Science',
+        'Technology',
+        'Sports',
+        'Sports_Golf',
+        'Sports_MLB',
+        'Sports_NBA',
+        'Sports_NFL',
+        'Sports_NHL',
+        'Sports_Soccer',
+        'Sports_Tennis',
+        'Sports_CFB',
+        'Sports_CBB',
+        'US',
+        'US_Northeast',
+        'US_South',
+        'US_Midwest',
+        'US_West',
+        'World',
+        'World_Africa',
+        'World_Americas',
+        'World_Asia',
+        'World_Europe',
+        'World_MiddleEast',
+    )
+    NEWS_CATEGORIES_GB = (
+        'Business',
+        'Entertainment',
+        'Health',
+        'Politics',
+        'ScienceAndTechnology',
+        'Sports',
+        'UK',
+        'World',
+    )
